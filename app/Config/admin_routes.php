87379043f07816d9ec220d2273392c401e893088
homeworkfree <lemaibk08@gmail.com>
2013-11-04 22:30:46 +0700

    dashboard router

--- app/Config/admin_routes.php
+++ app/Config/admin_routes.php
@@ -4,7 +4,7 @@
  * Routers for backend
  */
 
-Router::connect('/admin/dashboard', array('controller' => 'admin', 'action' => 'index','admin'=>true));
+Router::connect('/dashboard', array('controller' => 'admin', 'action' => 'index','admin'=>true));
 Router::connect('/admin/danh-muc', array('controller' => 'categories', 'action' => 'index', 'admin' => true));
 Router::connect('/admin/them-danh-muc', array('controller' => 'categories', 'action' => 'add', 'admin' => true));
 Router::connect('/admin/danh-sach-bai-viet', array('controller' => 'posts', 'action' => 'index', 'admin' => true));
